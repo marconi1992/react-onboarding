0884eb2ad6c4d176334bd6cb3f099a11fb417070
Felipe Guizar Diaz <felipegaiacharly@gmail.com>
2018-06-06 19:34:14 -0500

    Add babel react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const heading = React.createElement('h1', { key: 0 }, 'React Vanilla')
-const button = React.createElement('button', { key: 1 }, 'button')
-const container = React.createElement('div', {}, [ heading, button ])
-
 ReactDOM.render(
-  container,
+  <div>
+    <h1>React JSX</h1>
+    <button>Button</button>
+  </div>,
   document.getElementById('app')
 )
