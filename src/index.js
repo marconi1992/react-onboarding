51383ab9edef3b5d548887d81284f94341f481c9
Felipe Guizar Diaz <felipegaiacharly@gmail.com>
2018-06-06 19:28:45 -0500

    Add react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-const button = document.createElement('button')
-button.textContent = 'Button'
-document.body.appendChild(button)
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const heading = React.createElement('h1', { key: 0 }, 'React Vanilla')
+const button = React.createElement('button', { key: 1 }, 'button')
+const container = React.createElement('div', {}, [ heading, button ])
+
+ReactDOM.render(
+  container,
+  document.getElementById('app')
+)
